96adaa4961d37f0fc0bebe5077e21d1c708607b4
Furkan Yaglidere <115979551+11Furkan@users.noreply.github.com>
2023-08-18 12:51:04 +0200

    Update test_neu.py

--- test_neu.py
+++ test_neu.py
@@ -4,7 +4,7 @@ from neu import multi
 
 class TestMulti(unittest.TestCase):
     
-    def Test_multi_function(self):
+    def test_multi_function(self):
         self.assertEqual(multi(2,4),8)
         self.assertEqual(multi(2,5),10)
 
