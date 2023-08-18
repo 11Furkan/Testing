12d2c711acffd61264eb969f4b5452b79cef6f91
Furkan Yaglidere <115979551+11Furkan@users.noreply.github.com>
2023-08-18 12:29:25 +0200

    Update neu.py

--- neu.py
+++ neu.py
@@ -3,9 +3,10 @@ def multi(p: int, q: int) -> int:
 
     print("Multiplication of two numbers:", result)
 
-p = int(input("Enter first number: "))
-q = int(input("Enter second number: "))
-
+# p = int(input("Enter first number: "))
+# q = int(input("Enter second number: "))
+p = 4
+q = 5
 multi(p, q)
 
 
