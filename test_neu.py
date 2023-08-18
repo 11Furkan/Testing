902a072a5825da830fa8d6fde729311b7726db53
Furkan Yaglidere <115979551+11Furkan@users.noreply.github.com>
2023-08-18 12:52:19 +0200

    Update test_neu.py

--- test_neu.py
+++ test_neu.py
@@ -6,7 +6,7 @@ class TestMulti(unittest.TestCase):
     
     def test_multi_function(self):
         self.assertEqual(multi(2,4),8)
-        self.assertEqual(multi(2,5),10)
+        self.assertEqual(multi(2,5),11)
 
 if __name__ == "__main__":
     unittest.main()
